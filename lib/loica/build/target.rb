f14d2951ab2c60f040b95b4e3534844dc092c237
Seba Gamboa <me@sagmor.com>
2015-08-14 20:28:49 -0300

    Working on targets

--- lib/loica/build/target.rb
+++ lib/loica/build/target.rb
@@ -3,9 +3,38 @@ module Loica::Build # :nodoc:
   # This represent's an application target build
   class Target < Configuration
 
+    # Raised when the target's application is unknown
+    ApplicationUnknownError = Class.new(Error)
+
+    # Raised when the target's platform is unknown
+    PlatformUnknownError = Class.new(Error)
+
     # The target's application
     #
     # @return [Loica::Build::Application] the application
-    attr_accessor :application
+    def application
+      raise ApplicationUnknownError unless @application
+
+      @application
+    end
+
+    # @see #application
+    def application=(application)
+      @application = application
+    end
+
+    # The target's target platform
+    #
+    # @return [Loica::Build::Platform] the platform
+    def platform
+      raise PlatformUnknownError unless @platform
+
+      @platform
+    end
+
+    # @see #platform
+    def platform=(platform)
+      @platform = platform
+    end
   end
 end
--- spec/loica/build/target_spec.rb
+++ spec/loica/build/target_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Loica::Build::Target do
+  describe "#platform" do
+    it "raises an error when unknown" do
+      expect{
+        Loica::Build::Target.new.platform
+      }.to raise_error(Loica::Build::Target::PlatformUnknownError)
+    end
+  end
+
+  describe "#application" do
+    it "raises an error when unknown" do
+      expect{
+        Loica::Build::Target.new.application
+      }.to raise_error(Loica::Build::Target::ApplicationUnknownError)
+    end
+  end
 end
