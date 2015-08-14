7fd3948940650b2b90ce8f53835ed400cf2dc94d
Seba Gamboa <me@sagmor.com>
2015-08-14 14:25:41 -0300

    bundle gem

---
+++ lib/loica/build.rb
@@ -0,0 +1,7 @@
+require "loica/build/version"
+
+module Loica
+  module Build
+    # Your code goes here...
+  end
+end
---
+++ lib/loica/build/version.rb
@@ -0,0 +1,5 @@
+module Loica
+  module Build
+    VERSION = "0.1.0"
+  end
+end
