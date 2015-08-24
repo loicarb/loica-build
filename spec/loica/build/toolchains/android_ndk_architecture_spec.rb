ebdff75fdb24dae8e4d81ba2a9fe71d4a2d10a07
Seba Gamboa <me@sagmor.com>
2015-08-23 21:19:29 -0300

    Spec formatting

--- spec/loica/build/toolchains/android_ndk_architecture_spec.rb
+++ spec/loica/build/toolchains/android_ndk_architecture_spec.rb
@@ -3,24 +3,27 @@ require 'spec_helper'
 describe Loica::Build::Toolchains::AndroidNDK::Architecture do
   describe "#initialize" do
     it "requires a valid architecture" do
-      expect{ described_class.new({arch: 'wrong-arch'}) }.to raise_error(ArgumentError)
-      expect{ described_class.new({arch: 'armeabi'}) }.not_to raise_error(ArgumentError)
+      expect{
+        described_class.new({arch: 'wrong-arch'})
+      }.to raise_error(ArgumentError)
+
+      expect{
+        described_class.new({arch: 'armeabi'})
+      }.not_to raise_error(ArgumentError)
     end
 
     it "requires a valid toolchain" do
-      expect{ described_class.new({
-        arch: 'armeabi'
-      }) }.not_to raise_error(ArgumentError)
+      expect{
+        described_class.new({arch: 'armeabi'})
+      }.not_to raise_error(ArgumentError)
 
-      expect{ described_class.new({
-        arch: 'armeabi',
-        toolchain: :clang
-      }) }.not_to raise_error(ArgumentError)
+      expect{
+        described_class.new({arch: 'armeabi', toolchain: :clang})
+      }.not_to raise_error(ArgumentError)
 
-      expect{ described_class.new({
-        arch: 'armeabi',
-        toolchain: :wrong
-      }) }.to raise_error(ArgumentError)
+      expect{
+        described_class.new({arch: 'armeabi',toolchain: :wrong})
+      }.to raise_error(ArgumentError)
     end
 
   end
